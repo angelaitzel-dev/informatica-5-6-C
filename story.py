65a2accb65520a61c2c9aabe796d714b00747f9b
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-08-14 16:54:44 +0000

    Upper worked

--- story.py
+++ story.py
@@ -25,7 +25,7 @@ def main():
     print()
     print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
     print()
-    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.".upper())
 
 if __name__ == "__main__":
     main()
