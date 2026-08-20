147fa6d5e138a5a48c7e1c664b9c9f42ba4b7be3
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-08-20 17:26:50 +0000

    Moore2

--- moores.py
+++ moores.py
@@ -1,10 +1,17 @@
 def main():
     years= int(input("How many years do you want to see into the future?: "))
     transistors = 178000002000
-    years /= 2
-    transistors *= (2**years)
-    transistors = round(transistors)
-    print(f"This is the prediction for the future: {transistors}")
+    current_year = 2026
+
+
+    if (current_year + years) >= 2030:
+        print("The law is not valid.")
+    else:
+        years /= 2
+        transistors *= (2**years)
+        transistors = round(transistors)
+        print(f"This is the prediction for the future: {transistors}")
+
 
 if __name__ == "__main__":
     main()
