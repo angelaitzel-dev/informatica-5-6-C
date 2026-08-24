33947cf99680db136590910395de786399c4a25b
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-08-24 19:37:57 +0000

    passwords

---
+++ password.py
@@ -0,0 +1,10 @@
+def main():
+
+    password = "Securepassword"
+    attempt = input("Guess the Password:")
+    if attempt == password:
+            print("You guessed the password!")
+    print("End of program.")
+
+if __name__ == "__main__":
+    main()
