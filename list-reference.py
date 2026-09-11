d792b3dc6494545526d0679e9a37225166136aa3
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-09-11 17:31:59 +0000

    list

--- list-reference.py
+++ list-reference.py
@@ -17,6 +17,26 @@ def main():
     lista.remove("Rojo")
     print("con remove(´Rojo´):",lista)
 
+    #Blue
+    number = [1, 4, 5, 7, 9, 3]
+    number.sort()
+    print(number)
+
+    #Yellow
+    fruits = ["apple", "orange", "grapes"]
+    fruits.append("banana")
+    print(fruits)
+
+    fruits1 = ["Apple", "Orange", "grapes"]
+    fruits1.insert(2,"banana")
+    print(fruits1)
+
+    #green
+
+    objects = ["Pencil", "Computer", "chair", "paper", "yoyo", "nickel"]
+    objects[len(objects)-1] = "Rock"
+    print(objects)
+    print(len(objects))
 
 if __name__ == "__main__":
     main()
