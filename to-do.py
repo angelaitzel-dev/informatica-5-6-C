1a7ec18080d0839d5d207f85e663b33d72cd2e24
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-09-15 17:30:26 +0000

    lists

---
+++ to-do.py
@@ -0,0 +1,29 @@
+
+def main():
+    tasks= []
+    answer= ""
+    ask = ""
+    while True:
+        answer = input("Do you want to: complete a task, add a task, or exit? ").strip().lower()
+        if answer == "add":
+            tasks.append(input("Add a task to the list: "))
+            print(tasks)
+        elif answer == "complete":
+            ask = input("Have you finshed all of them?").strip().lower()
+            if ask == "yes":
+                print("You finished all of them!")
+                print(tasks)
+                break
+            else:
+                tasks.remove(input("What did you finish?"))
+                print(tasks)
+            if tasks == []:
+                print("You finished!")
+                break
+        elif answer == "exit":
+            break
+        else:
+            print("Why are you like this?")
+            break
+if __name__ == "__main__":
+    main()
